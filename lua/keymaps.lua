fdd7820240a9859f986c5952b98d291a50fb4635
Vladimir Figueroa <vladimir.antonio.f@gmail.com>
2023-01-22 01:26:19 -0300

    Configure key mappings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,72 +1,96 @@
 local map = vim.api.nvim_set_keymap
 local lmap = vim.keymap.set
 local fns = require('functions')
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 
 -- Map leader key to space
 vim.g.mapleader = ' '
 
+-------------
+-- General --
+-------------
+-- Switch between windows
+map('n', '<C-h>', '<C-w>h', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-l>', '<C-w>l', opts)
+
+-- Search within insert mode
+map('i', '<A-n>', '<Esc>`^ni', opts)
+map('i', '<A-N>', '<Esc>`^Ni', opts)
+
+-- Don't copy the replaced text after pasting in visual mode
+-- https://vim.fandom.com/wiki/Replace_a_word_with_yanked_text#Alternative_mapping_for_paste
+map('x', 'p', 'p:let @+=@0<CR>:let @"=@0<CR>gv', opts)
+map('x', 'P', 'p:let @+=@0<CR>:let @"=@0<CRPgv', opts)
+
+-- Select all text in current buffer
+map('', '<leader>a', ':keepjumps normal! ggVG<CR>', opts)
+
+-- Duplicate current line
+map('n', '<C-d>', ':copy .<CR>k', opts)
+map('i', '<C-d>', '<Esc>:copy .<CR>gi', opts)
+-- Duplicate in Visual Mode
+map('v', '<C-d>', '""y`>""pgv', opts)
+-- Duplicate in Visual Block Mode
+map('x', '<C-d>', '""ygvA<Esc>""pgv', opts)
+
+-- Move current line / block down (Alt+j)
+map('n', '<M-j>', ":m .+1<CR>==", opts)
+map('i', '<M-j>', "<Esc>:m .+1<CR>==gi", opts)
+map('x', '<M-j>', ":m '>+1<CR>gv-gv", opts)
+-- Move current line / block up (Alt+k)
+map('n', '<M-k>', ":m .-2<CR>==", opts)
+map('i', '<M-k>', "<Esc>:m .-2<CR>==gi", opts)
+map('x', '<M-k>', ":m '<-2<CR>gv-gv", opts)
+
 -----------
 -- Files --
 -----------
--- New file
-map('n', '<leader>nn', ':e newfile<cr>', { noremap = true, silent = true })
 -- Write current file
-map('n', '<leader>ww', ':w<cr>', { noremap = true })
+map('n', '<C-s>', ':w<CR>', { noremap = true })
+map('i', '<C-s>', '<Esc>:w<CR>gi', { noremap = true })
+map('x', '<C-s>', '<Esc>:w<CR>gv', { noremap = true })
+-- Write all files (Ctrl+K+S)
+map('n', '<C-k><C-s>', ':wa<CR>', { noremap = true })
+map('i', '<C-k><C-s>', '<Esc>:wa<CR>gi', { noremap = true })
+map('x', '<C-k><C-s>', '<Esc>:wa<CR>gv', { noremap = true })
 -- Write current file and quit
-map('n', '<leader>wq', ':wq<cr>', { noremap = true })
+map('n', '<leader>wq', ':wq<CR>', { noremap = true })
 -- Write current file and quit all files
-map('n', '<leader>wa', ':wqa<cr>', { noremap = true })
+map('n', '<leader>wa', ':wqa<CR>', { noremap = true })
 -- Quit file
-map('n', '<leader>qq', ':q<cr>', { noremap = true })
+map('n', '<leader>qq', ':q<CR>', { noremap = true })
 -- Force quit file
-map('n', '<leader>QQ', ':q!<cr>', { noremap = true })
+map('n', '<leader>QQ', ':q!<CR>', { noremap = true })
 -- Quit all files
-map('n', '<leader>qa', ':qa<cr>', { noremap = true })
+map('n', '<leader>qa', ':qa<CR>', { noremap = true })
 -- Force quit all files
-map('n', '<leader>QA', ':qa!<cr>', { noremap = true })
+map('n', '<leader>QA', ':qa!<CR>', { noremap = true })
+
 -- copy result to the system clipboard and echo the result
 -- the cb> prompt means the clipboard
 -- *f*ile *n*ame, ex. init.vim
-map('', '<Leader>fn', ':let @+ = expand("%:t") | echo "cb> " . @+<CR>', { noremap = true })
+map('n', '<leader>cfn', ':let @+ = expand("%:t") | echo "cb> " . @+<CR>', { noremap = true })
 -- *f*ile *p*ath, ex. /home/user/nvim/init.vim
-map('', '<Leader>fp', ':let @+ = expand("%:p") | echo "cb> " . @+<CR>', { noremap = true })
+map('n', '<leader>cfp', ':let @+ = expand("%:p") | echo "cb> " . @+<CR>', { noremap = true })
 -- *d*irectory *p*ath, ex. /home/user/nvim
-map('', '<Leader>dp', ':let @+ = expand("%:p:h") | echo "cb> " . @+<CR>', { noremap = true })
+map('n', '<leader>cdp', ':let @+ = expand("%:p:h") | echo "cb> " . @+<CR>', { noremap = true })
 -- *d*irectory *n*ame, ex. nvim
-map('', '<Leader>dn', ':let @+ = expand("%:p:h:t") | echo "cb> " . @+<CR>', { noremap = true })
-
--- Go to first character in line
-lmap('', '<leader>h', '^')
-
--- Go to last character in line
-lmap('', '<leader>l', 'g_')
-
--- Select all text in current buffer
-map('n', '<leader>aa', ':keepjumps normal! ggVG<CR>', { noremap = true })
-
--- Mappings for 'D|C' to use unnamed register
-map('n', '<leader>d', '"_d', { noremap = true })
-map('n', '<leader>D', '"_D', { noremap = true })
-map('n', '<leader>c', '"_c', { noremap = true })
-map('n', '<leader>C', '"_C', { noremap = true })
-
--- Move selected lines up/down
-map('x', 'K', ":move '<-2<cr>gv=gv", { noremap = true })
-map('x', 'J', ":move '>+1<cr>gv=gv", { noremap = true })
+map('n', '<leader>cdn', ':let @+ = expand("%:p:h:t") | echo "cb> " . @+<CR>', { noremap = true })
 
 -------------
 -- Buffers --
 -------------
 -- Quick buffer navigation (next/previous)
-map('n', '<leader>bh', ':bprev<cr>', { noremap = true, silent = true })
-map('n', '<leader>bl', ':bnext<cr>', { noremap = true, silent = true })
+map('n', '<leader>bh', ':bprev<CR>', opts)
+map('n', '<leader>bl', ':bnext<CR>', opts)
 
--- Close current buffer
-map('n', '<leader>bq', ':bdelete<cr>', { noremap = true, silent = true })
+-- New buffer
+map('n', '<leader>bn', ':enew<CR>', opts)
 
--- Close buffer while preserving the layout
-lmap('n', '<leader>bc', fns.close_current_buffer, { noremap = true, silent = true })
+-- Close current buffer while preserving the layout
+lmap('n', '<leader>bc', fns.close_current_buffer, opts)
 
 -------------
 -- Plugins --
@@ -113,36 +137,36 @@ end
 function BufferlineKeymaps(bufferline)
   -- Navigate listed buffers with <leader>1-9
   for i = 1, 9 do
-    lmap('n', '<leader>' .. i, function () bufferline.go_to_buffer(i, true) end, { noremap = true, silent = true })
+    lmap('n', '<leader>' .. i, function () bufferline.go_to_buffer(i, true) end, opts)
   end
 end
 
 -- nvim-tree
 function NvimTreeKeymaps()
-  map('n', '<leader>mm', '<cmd>NvimTreeToggle<cr>', { noremap = true })
-  map('n', '<leader>mf', '<cmd>NvimTreeFocus<cr>', { noremap = true })
+  map('n', '<leader>mm', ':NvimTreeToggle<cr>', { noremap = true })
+  map('n', '<leader>mf', ':NvimTreeFocus<cr>', { noremap = true })
 end
 
 -- Telescope
 function TelescopeKeymaps()
-  map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true })
-  map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true })
-  map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true })
-  map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true })
+  map('n', '<leader>ff', ':Telescope find_files<cr>', { noremap = true })
+  map('n', '<leader>fg', ':Telescope live_grep<cr>', { noremap = true })
+  map('n', '<leader>fb', ':Telescope buffers<cr>', { noremap = true })
+  map('n', '<leader>fh', ':Telescope help_tags<cr>', { noremap = true })
 
   -- Show key bindings list
-  map('n', '<leader>fk', '<cmd>Telescope keymaps<cr>', { noremap = true })
+  map('n', '<leader>fk', ':Telescope keymaps<cr>', { noremap = true })
 
   -- Show symbols list
-  map('n', '<leader>fs', '<cmd>Telescope symbols<cr>', { noremap = true })
+  map('n', '<leader>fs', ':Telescope symbols<cr>', { noremap = true })
 end
 
 -- Trouble
 function TroubleKeymaps()
-  map('n', '<leader>xx', '<cmd>TroubleToggle<cr>', { silent = true, noremap = true })
-  map('n', '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>', { silent = true, noremap = true })
-  map('n', '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>', { silent = true, noremap = true })
-  map('n', '<leader>xl', '<cmd>TroubleToggle loclist<cr>', { silent = true, noremap = true })
-  map('n', '<leader>xq', '<cmd>TroubleToggle quickfix<cr>', { silent = true, noremap = true })
-  map('n', 'gR', '<cmd>TroubleToggle lsp_references<cr>', { silent = true, noremap = true })
+  map('n', '<leader>xx', ':TroubleToggle<cr>', opts)
+  map('n', '<leader>xw', ':TroubleToggle workspace_diagnostics<cr>', opts)
+  map('n', '<leader>xd', ':TroubleToggle document_diagnostics<cr>', opts)
+  map('n', '<leader>xl', ':TroubleToggle loclist<cr>', opts)
+  map('n', '<leader>xq', ':TroubleToggle quickfix<cr>', opts)
+  map('n', 'gR', ':TroubleToggle lsp_references<cr>', opts)
 end
