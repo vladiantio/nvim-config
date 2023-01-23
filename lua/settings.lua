875bf9757abb2eaa50de1878c55505b7c1061288
Vladimir Figueroa <vladimir.antonio.f@gmail.com>
2023-01-23 01:21:04 -0300

    Settings: Disable some built-in plugins

--- lua/settings.lua
+++ lua/settings.lua
@@ -81,3 +81,39 @@ vim.o.showbreak      = '↪ '    -- Wrapped line indicator
 vim.o.listchars      = 'eol:↲,nbsp:␣,tab:▸→,extends:❯,precedes:❮,trail:∙' -- Characters to display for invisible characters
 vim.o.fillchars      = 'eob: ' -- Customise fill characters
 
+------------
+-- Others --
+------------
+-- Disable some built-in plugins
+local disabled_plugins = {
+  "2html_plugin",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "matchit",
+  "tar",
+  "tarPlugin",
+  "rrhelper",
+  "spellfile_plugin",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin",
+  "tutor",
+  "rplugin",
+  "syntax",
+  "synmenu",
+  "optwin",
+  "compiler",
+  "bugreport",
+  "ftplugin",
+}
+
+for _, plugin in pairs(disabled_plugins) do
+  vim.g["loaded_" .. plugin] = 1
+end
